8ccad814e5b709d93d7eed1e57c8f9602256b969
ilya-sk <ilyarus@inbox.ru>
2020-05-28 13:29:32 +0300

    add lesson 10

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,20 @@
 $(function(){
 
+    $(".rate-star").rateYo({
+        rating: 4.6,
+        starWidth: "14px",
+        readOnly: true
+      });
 
+
+    var mixer = mixitup('.product__inner-box');
+
+    $('.slaider-content').slick({
+        prevArrow: '<button type="button" class="slick-arrow slick-arrow--prev"></span></button>',
+        nextArrow: '<button type="button" class="slick-arrow slick-arrow--next"></button>',
+        appendArrows: '.slaider-arrows',
+
+      });
 
 
     
